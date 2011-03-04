15d295c89b32b3d6ee93be18fef46198c5626747
Piotrek Okoński <piotrek@okonski.org>
2011-03-04 16:04:37 +0100

    Clean up the code and allow tracked :user to be Proc or a Symbol/String, setting activity_user on a tracked object takes priority over :user attribute in model definition

--- lib/keep_track/creation.rb
+++ lib/keep_track/creation.rb
@@ -4,17 +4,19 @@ module KeepTrack
     
     included do
       after_create do
-        user = nil
-        if not self.activity_user.nil?
+        if self.activity_user
           user = self.activity_user
         else
-          if self.class.activity_user_global.is_a?(Symbol) or self.class.activity_user_global.is_a?(String)
-            user = self[self.class.activity_user_global]
-          end          
+          case self.activity_user_global
+            when Symbol, String 
+              user = self[self.class.activity_user_global]
+            when Proc
+              user = self.class.activity_user_global.call           
+          end
         end
+
         self.activities.create(:key => self.class.name.downcase+".create", :user_id => user, :parameters => self.activity_params)
       end
-    end
-      
+    end   
   end
 end
