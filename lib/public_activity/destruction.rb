ce0a0c673dabf5f6b59842d23584f0c93ae87981
Piotrek Okoński <piotrek@okonski.org>
2011-04-09 16:09:43 +0200

    Paremeterize class names in keys instead of downcasing

--- lib/public_activity/destruction.rb
+++ lib/public_activity/destruction.rb
@@ -11,7 +11,7 @@ module PublicActivity
         # Records an activity upon destruction of the tracked model
         def activity_on_destroy
           settings = prepare_settings
-          create_activity("activity."+self.class.name.downcase+".destroy", settings[:owner], settings[:parameters])
+          create_activity("activity."+self.class.name.parameterize('_')+".destroy", settings[:owner], settings[:parameters])
         end
     end
   end
