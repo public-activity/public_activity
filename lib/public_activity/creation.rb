71222c0c43afcf8ff1edc17ad67bfb31489dd71f
Shawn Bower <shawn.bower@gmail.com>
2011-10-18 13:11:33 -0400

    breaking out update

--- lib/public_activity/creation.rb
+++ lib/public_activity/creation.rb
@@ -5,7 +5,6 @@ module PublicActivity
 
     included do
       after_create :activity_on_create
-      after_update :activity_on_update
     end   
     # Handlers responsible for creating Activities.
     module InstanceMethods
@@ -14,12 +13,6 @@ module PublicActivity
         def activity_on_create
           settings = prepare_settings
           create_activity(settings[:key] || "activity."+self.class.name.parameterize('_')+".create", settings[:owner], settings[:parameters])
-        end
-        
-        # Creates activity upon modification of the tracked model
-        def activity_on_update
-          settings = prepare_settings
-          create_activity(settings[:key] || "activity."+self.class.name.parameterize('_')+".update", settings[:owner], settings[:parameters])
         end
     end
   end
