e8e11671c83b422c44c8c68b4bb86a62e5b3f124
Piotrek Okoński <piotrek@okonski.org>
2012-07-12 22:45:58 +0200

    Move ClassMethods to the bottom to structure the code properly

--- lib/public_activity/tracked.rb
+++ lib/public_activity/tracked.rb
@@ -67,70 +67,6 @@ module PublicActivity
     attr_accessor :activity_key
     @activity_key = nil
     
-    # Module with basic +tracked+ method that enables tracking models.
-    module ClassMethods
-      # Adds required callbacks for creating and updating
-      # tracked models and adds +activities+ relation for listing
-      # associated activities.
-      # 
-      # == Parameters:
-      # [:owner]
-      #   Specify the owner of the {Activity} (person responsible for the action).
-      #   It can be a Proc, Symbol or an ActiveRecord object:
-      #   == Examples:
-      #    @article.activity :owner => :author    
-      #    @article.activity :owner => {|o| o.author}
-      #    @article.activity :owner => User.where(:login => 'piotrek').first
-      #   Keep in mind that owner relation is polymorphic, so you can't just provide id number of the owner object.
-      # [:params]
-      #   Accepts a Hash with custom parameters you want to pass to i18n.translate
-      #   method. It is later used in {Activity#text} method.
-      #   == Example:
-      #    @article.activity :parameters => {:title => @article.title, :short => truncate(@article.text, :length => 50)}
-      #   Everything specified here has a lower priority than parameters specified directly in {#activity} method.
-      #   So treat it as a place where you provide 'default' values.
-      #   For more dynamic settings refer to {Activity} model 
-      #   documentation.
-      # [:skip_defaults]
-      #   Disables recording of activities on create/update/destroy leaving that to programmer's choice. Check {PublicActivity::Common#create_activity}
-      #   for a guide on how to manually record activities.
-      def tracked(options = {})
-        include Common
-        
-        all_options = [:create, :update, :destroy]
-        
-        if !options[:skip_defaults] && !options[:only] && !options[:except] 
-          include Creation
-          include Destruction
-          include Update
-        end
-        
-        if options[:except].is_a? Array
-          options[:only] = all_options - options[:except]
-        end
-        
-        if options[:only].is_a? Array
-            options[:only].each do |opt|
-              if opt.eql?(:create)
-                include Creation
-              elsif opt.eql?(:destroy)
-                include Destruction
-              elsif opt.eql?(:update)
-                include Update
-              end
-            end
-        end
-            
-        if options[:owner]
-          self.activity_owner_global = options[:owner]
-        end
-        if options[:params]
-          self.activity_params_global = options[:params]
-        end
-        has_many :activities, :class_name => "PublicActivity::Activity", :as => :trackable      
-      end
-    end
-    
     # A shortcut method for setting custom key, owner and parameters of {Activity}
     # in one line. Accepts a hash with 3 keys:
     # :key, :owner, :params. You can specify all of them or just the ones you want to overwrite.
@@ -172,6 +108,70 @@ module PublicActivity
       self.activity_key = options[:key] if options[:key]
       self.activity_owner = options[:owner] if options[:owner]
       self.activity_params = options[:params] if options[:params]
+    end
+
+    # Module with basic +tracked+ method that enables tracking models.
+    module ClassMethods
+      # Adds required callbacks for creating and updating
+      # tracked models and adds +activities+ relation for listing
+      # associated activities.
+      #
+      # == Parameters:
+      # [:owner]
+      #   Specify the owner of the {Activity} (person responsible for the action).
+      #   It can be a Proc, Symbol or an ActiveRecord object:
+      #   == Examples:
+      #    @article.activity :owner => :author
+      #    @article.activity :owner => {|o| o.author}
+      #    @article.activity :owner => User.where(:login => 'piotrek').first
+      #   Keep in mind that owner relation is polymorphic, so you can't just provide id number of the owner object.
+      # [:params]
+      #   Accepts a Hash with custom parameters you want to pass to i18n.translate
+      #   method. It is later used in {Activity#text} method.
+      #   == Example:
+      #    @article.activity :parameters => {:title => @article.title, :short => truncate(@article.text, :length => 50)}
+      #   Everything specified here has a lower priority than parameters specified directly in {#activity} method.
+      #   So treat it as a place where you provide 'default' values.
+      #   For more dynamic settings refer to {Activity} model
+      #   documentation.
+      # [:skip_defaults]
+      #   Disables recording of activities on create/update/destroy leaving that to programmer's choice. Check {PublicActivity::Common#create_activity}
+      #   for a guide on how to manually record activities.
+      def tracked(options = {})
+        include Common
+
+        all_options = [:create, :update, :destroy]
+
+        if !options[:skip_defaults] && !options[:only] && !options[:except]
+          include Creation
+          include Destruction
+          include Update
+        end
+
+        if options[:except].is_a? Array
+          options[:only] = all_options - options[:except]
+        end
+
+        if options[:only].is_a? Array
+          options[:only].each do |opt|
+            if opt.eql?(:create)
+              include Creation
+            elsif opt.eql?(:destroy)
+              include Destruction
+            elsif opt.eql?(:update)
+              include Update
+            end
+          end
+        end
+
+        if options[:owner]
+          self.activity_owner_global = options[:owner]
+        end
+        if options[:params]
+          self.activity_params_global = options[:params]
+        end
+        has_many :activities, :class_name => "PublicActivity::Activity", :as => :trackable
+      end
     end
 
   end
