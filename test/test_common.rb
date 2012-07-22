fccf32612ecbae23eb1ee26a655dba3741e32305
Piotrek Okoński <piotrek@okonski.org>
2012-07-22 18:26:20 +0200

    Add test for inheriting parameters

--- test/test_common.rb
+++ test/test_common.rb
@@ -12,4 +12,16 @@ class TestCommon < MiniTest::Unit::TestCase
     assert_equal activity.parameters[:author_name], options[:params][:author_name]
     assert_equal activity.owner.id, owner.id
   end
+
+  def test_params_inheriting
+    options = {:params => {:author_name => "Peter", :summary => "Default summary goes here..."}}
+    klass = article(options)
+    article = klass.new
+    article.activity :params => {:author_name => "Michael"}
+    article.save
+    activity = article.activities.last
+
+    assert_equal activity.parameters[:author_name], "Michael"
+    assert_equal activity.parameters[:summary], options[:params][:summary]
+  end
 end
