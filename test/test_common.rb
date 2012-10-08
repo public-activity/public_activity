f3d14754b1e3b978c7f38d703c758585b1d52e88
Piotrek Okoński <piotrek@okonski.org>
2012-10-08 23:05:47 +0200

    Remove whitespace

--- test/test_common.rb
+++ test/test_common.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TestCommon < MiniTest::Unit::TestCase
-  
+
   def test_default_settings
     owner = User.create(:name => "Peter Pan")
     options = {:params => {:author_name => "Peter", :article_name => :name, :short_article_name => proc {|c, m| m.name[0..3] + "..."}}, :owner => owner}
