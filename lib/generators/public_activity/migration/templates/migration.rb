1a676856a15a26a4ca206fa7f685a8ff9c70db00
Daniel Evans <evans.daniel.n@gmail.com>
2013-01-30 17:08:00 -0700

    Add _id,_type index pairs to activities table for improved query times

--- lib/generators/public_activity/migration/templates/migration.rb
+++ lib/generators/public_activity/migration/templates/migration.rb
@@ -11,6 +11,10 @@ class CreateActivities < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    add_index :activities, [:trackable_id, :trackable_type]
+    add_index :activities, [:owner_id, :owner_type]
+    add_index :activities, [:recipient_id, :recipient_type]
   end
   # Drop table
   def self.down
