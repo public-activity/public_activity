34bd844f41e059e217189adf18fbb28ce2ab53e2
Matt Gibson <downrightlies@gmail.com>
2014-01-08 00:03:09 +0000

    switched the association name to the table name

--- lib/public_activity/orm/active_record/trackable.rb
+++ lib/public_activity/orm/active_record/trackable.rb
@@ -7,7 +7,7 @@ module PublicActivity
         # Creates an association for activities where self is the *trackable*
         # object.
         def self.extended(base)
-          base.has_many :activities, :class_name => "::PublicActivity::Activity", :as => :trackable
+          base.has_many PublicActivity.config.table_name.to_sym, :class_name => "::PublicActivity::Activity", :as => :trackable
         end
       end
     end
