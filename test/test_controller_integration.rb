50e93c4bd3056b3892c3a43c6d33ea81d6451800
Jakub Okoński <jakub@okonski.org>
2015-01-23 17:01:28 +0100

    simplify StoreController implementation with Thread.current

--- test/test_controller_integration.rb
+++ test/test_controller_integration.rb
@@ -9,19 +9,23 @@ describe PublicActivity::StoreController do
   it 'stores controller' do
     controller = StoringController.new
     PublicActivity.set_controller(controller)
-    controller.must_be_same_as PublicActivity.instance_eval { class_variable_get(:@@controllers)[Thread.current.object_id] }
+    controller.must_be_same_as Thread.current[:public_activity_controller]
     controller.must_be_same_as PublicActivity.get_controller
   end
 
   it 'stores controller with a filter in controller' do
     controller = StoringController.new
-    controller._process_action_callbacks.select {|c| c.kind == :before}.map(&:filter).must_include :store_controller_for_public_activity
+
+    controller._process_action_callbacks
+      .select {|c| c.kind == :before }
+      .map(&:filter)
+      .must_include :store_controller_for_public_activity
+
     controller.instance_eval { store_controller_for_public_activity }
-    controller.must_be_same_as PublicActivity.class_eval { class_variable_get(:@@controllers)[Thread.current.object_id] }
+    controller.must_be_same_as Thread.current[:public_activity_controller]
   end
 
   it 'stores controller in a threadsafe way' do
-    reset_controllers
     PublicActivity.set_controller(1)
     PublicActivity.get_controller.must_equal 1
 
@@ -31,11 +35,5 @@ describe PublicActivity::StoreController do
     }
 
     PublicActivity.get_controller.must_equal 1
-    # cant really test finalizers though
-  end
-
-  private
-  def reset_controllers
-    PublicActivity.class_eval { class_variable_set(:@@controllers, {}) }
   end
 end
