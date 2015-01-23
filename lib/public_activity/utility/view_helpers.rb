c5fd1857ba74b389bea4dc05f18354752fe7c494
Jakub Okoński <jakub@okonski.org>
2015-01-23 17:16:50 +0100

    remove single_content_for helper

--- lib/public_activity/utility/view_helpers.rb
+++ lib/public_activity/utility/view_helpers.rb
@@ -11,13 +11,6 @@ module PublicActivity
     end
 
     alias_method :render_activities, :render_activity
-
-    # Helper for setting content_for in activity partial, needed to
-    # flush remains in between partial renders.
-    def single_content_for(name, content = nil, &block)
-      @view_flow.set(name, ActiveSupport::SafeBuffer.new)
-      content_for(name, content, &block)
-    end
   end
 
   ActionView::Base.include ViewHelpers
--- test/test_view_helpers.rb
+++ test/test_view_helpers.rb
@@ -16,21 +16,4 @@ describe 'ViewHelpers Rendering' do
     activity.expects(:render).with(self, {})
     render_activities([activity])
   end
-
-  it 'flushes content_for between partials renderes' do
-    @view_flow = mock('view_flow')
-    @view_flow.expects(:set).twice.with('name', ActiveSupport::SafeBuffer.new)
-
-    single_content_for('name', 'content')
-    @name.must_equal 'name'
-    @content.must_equal 'content'
-    single_content_for('name', 'content2')
-    @name.must_equal 'name'
-    @content.must_equal 'content2'
-  end
-
-  def content_for(name, content, &block)
-    @name = name
-    @content = content
-  end
 end
