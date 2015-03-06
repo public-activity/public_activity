3d155f22aad550b6715e9ff6a9f5f41f691db4a9
Piotrek Okoński <piotrek@okonski.org>
2015-03-06 19:40:59 +0100

    Revert "rework ORM dependencies to their gemfiles"
    This reverts commit b743d7f75d7292e0a7bbee8f94e14c73be0c07df.

--- run_tests.sh
+++ run_tests.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
-
 echo "Testing active_record 3.X:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.activerecord bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.activerecord COV=1 bundle exec rake
-
+rm -f Gemfile.lock;
+BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X bundle > /dev/null;
+COV=1 bundle exec rake;
 echo "Testing active_record 4.0:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.activerecord bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.activerecord COV=1 bundle exec rake
-
-echo "Testing mongoid rails 3.X:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.mongoid bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.mongoid COV=1 PA_ORM=mongoid bundle exec rake
-
-echo "Testing mongoid rails 4.0:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.mongoid bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.mongoid COV=1 PA_ORM=mongoid bundle exec rake
-
-echo "Testing mongo_mapper rails 3.X:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.mongo_mapper bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-3.X.mongo_mapper COV=1 PA_ORM=mongo_mapper bundle exec rake
-
-echo "Testing mongo_mapper rails 4.0:"
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.mongo_mapper bundle
-BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0.mongo_mapper COV=1 PA_ORM=mongo_mapper bundle exec rake
+rm -f Gemfile.lock;
+BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.0 bundle > /dev/null;
+COV=1 bundle exec rake;
+echo "Testing mongoid:";
+rm -f Gemfile.lock;
+PA_ORM=mongoid bundle > /dev/null;
+COV=1 PA_ORM=mongoid bundle exec rake;
+echo "Testing mongo_mapper:";
+rm -f Gemfile.lock;
+PA_ORM=mongo_mapper bundle > /dev/null;
+COV=1 PA_ORM=mongo_mapper bundle exec rake;
