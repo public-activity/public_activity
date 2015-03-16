bbc9ae858eb24efba2c497869168a961e77a6aca
Marcin Adamczyk <zolter89@gmail.com>
2015-03-16 14:05:38 +0100

    Update test_activity.rb

--- test/test_activity.rb
+++ test/test_activity.rb
@@ -6,12 +6,12 @@ I18n.config.backend.store_translations(:en,
 
 describe 'PublicActivity::Activity Rendering' do
   describe '#text' do
-    subject { PublicActivity::Activity.new(:key => 'activity.test', :parameters => {:one => 1}) }
+    subject { PublicActivity::Activity.new(:key => 'activity.test', :parameters => {:one => 1, "three" => 2}) }
 
     specify '#text uses translations' do
       subject.save
-      subject.text(:two => 2).must_equal('1 2')
-      subject.parameters.must_equal({:one => 1})
+      subject.text(:two => 2).must_equal('1 2 3')
+      subject.parameters.must_equal({:one => 1, "three" => 3})
     end
   end
 
