e61f58fee1ebab995f7ede5e02ac0e2717fb6d53
Ryan McGeary <ryan@mcgeary.org>
2016-03-17 16:30:21 -0600

    Refactor PublicActivity::StoreController
    * Use Thread.current variables instead of class variables, ObjectSpace,
      and finalizers
    
    Ref #252

--- test/test_controller_integration.rb
+++ test/test_controller_integration.rb
@@ -9,33 +9,32 @@ describe PublicActivity::StoreController do
   it 'stores controller' do
     controller = StoringController.new
     PublicActivity.set_controller(controller)
-    controller.must_be_same_as PublicActivity.instance_eval { class_variable_get(:@@controllers)[Thread.current.object_id] }
+    controller.must_be_same_as Thread.current[:public_activity_controller]
     controller.must_be_same_as PublicActivity.get_controller
   end
 
   it 'stores controller with a filter in controller' do
     controller = StoringController.new
-    controller._process_action_callbacks.select {|c| c.kind == :before}.map(&:filter).must_include :store_controller_for_public_activity
-    controller.instance_eval { store_controller_for_public_activity }
-    controller.must_be_same_as PublicActivity.class_eval { class_variable_get(:@@controllers)[Thread.current.object_id] }
+    controller._process_action_callbacks.select {|c| c.kind == :around}.map(&:filter).must_include :store_controller_for_public_activity
+    controller.instance_eval do
+      store_controller_for_public_activity do
+        controller.must_be_same_as PublicActivity.get_controller
+      end
+    end
   end
 
   it 'stores controller in a threadsafe way' do
-    reset_controllers
     PublicActivity.set_controller(1)
     PublicActivity.get_controller.must_equal 1
 
     a = Thread.new {
       PublicActivity.set_controller(2)
       PublicActivity.get_controller.must_equal 2
+      PublicActivity.set_controller(nil)
     }
 
     PublicActivity.get_controller.must_equal 1
-    # cant really test finalizers though
-  end
 
-  private
-  def reset_controllers
-    PublicActivity.class_eval { class_variable_set(:@@controllers, {}) }
+    PublicActivity.set_controller(nil)
   end
 end
