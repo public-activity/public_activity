5dc8cce38a574c2a0ccb57bdb9c8a2c897141dd0
Piotr Okoński <piotrek@okonski.org>
2017-02-12 13:30:08 +0100

    Separately configure associations for different Rails versions

--- lib/public_activity/orm/active_record/activity.rb
+++ lib/public_activity/orm/active_record/activity.rb
@@ -9,12 +9,22 @@ module PublicActivity
 
         # Define polymorphic association to the parent
         belongs_to :trackable, :polymorphic => true
-        with_options(::ActiveRecord::VERSION::MAJOR >= 5 ? { :required => false } : { }) do
+
+        case ::ActiveRecord::VERSION::MAJOR
+        when 3..4
           # Define ownership to a resource responsible for this activity
           belongs_to :owner, :polymorphic => true
           # Define ownership to a resource targeted by this activity
           belongs_to :recipient, :polymorphic => true
+        when 5
+          with_options(:required => false) do
+            # Define ownership to a resource responsible for this activity
+            belongs_to :owner, :polymorphic => true
+            # Define ownership to a resource targeted by this activity
+            belongs_to :recipient, :polymorphic => true
+          end
         end
+
         # Serialize parameters Hash
         serialize :parameters, Hash unless [:json, :jsonb, :hstore].include?(columns_hash['parameters'].type)
 
