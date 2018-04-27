public-activity/public_activity

commit ec2cbdb8ac588c6695738c6ac4bb16fb32235a49

Initial 1.6.0 version

lib/public_activity/orm/active_record/activity.rb
@@ -16,7 +16,7 @@ module PublicActivity
           belongs_to :recipient, :polymorphic => true
         end
         # Serialize parameters Hash
-        serialize :parameters, Hash
+        serialize :parameters, Hash unless [:json, :jsonb, :hstore].include?(columns_hash['parameters'].type)
 
         if ::ActiveRecord::VERSION::MAJOR < 4 || defined?(ProtectedAttributes)
           attr_accessible :key, :owner, :parameters, :recipient, :trackable

lib/public_activity/config.rb
@@ -6,14 +6,6 @@ module PublicActivity
     include ::Singleton
     attr_accessor :enabled, :table_name
 
-    @@orm = :active_record
-
-    def initialize
-      # Indicates whether PublicActivity is enabled globally
-      @enabled    = true
-      @table_name = "activities"
-    end
-
     # Evaluates given block to provide DSL configuration.
     # @example Initializer for Rails
     #   PublicActivity::Config.set do
@@ -24,14 +16,15 @@ module PublicActivity
     def self.set &block
       b = Block.new
       b.instance_eval &block
-      @@orm = b.orm unless b.orm.nil?
+      orm(b.orm) unless b.orm.nil?
       instance
-      instance.instance_variable_set(:@enabled,    b.enabled)     unless  b.enabled.nil?
+      Thread.current[:public_activity_enabled] = b.enabled unless b.enabled.nil?
+      Thread.current[:public_activity_table_name] = b.table_name unless b.enabled.nil?
     end
 
     # Set the ORM for use by PublicActivity.
     def self.orm(orm = nil)
-      @@orm = (orm ? orm.to_sym : false) || @@orm
+      Thread.current[:public_activity_orm] = (orm ? orm.to_sym : false) || Thread.current[:public_activity_orm]
     end
 
     # alias for {#orm}
@@ -44,6 +37,10 @@ module PublicActivity
     # @see Config#orm
     def orm(orm=nil)
       self.class.orm(orm)
+    end
+
+    def table_name
+      Thread.current[:public_activity_orm] || "activities"
     end
 
     # Provides simple DSL for the config block.
@@ -63,7 +60,7 @@ module PublicActivity
       # Sets the table_name
       # for the model
       def table_name(name = nil)
-        PublicActivity.config.table_name = name
+        @table_name = (name.nil? ? @table_name : name)
       end
     end
   end

lib/public_activity/utility/view_helpers.rb
@@ -21,6 +21,8 @@ module PublicActivity
       content_for(name, content, &block)
     end
   end
+end
 
-  ActionView::Base.class_eval { include ViewHelpers }
+ActiveSupport.on_load(:action_view) do
+  include PublicActivity::ViewHelpers
 end

commit f9da2d4f3ae67013cb167759745a6e6b04281447

Fix issue introduced in ec2cbdb while running db:schema:load

lib/public_activity/orm/active_record/activity.rb
@@ -26,7 +26,9 @@ module PublicActivity
         end
 
         # Serialize parameters Hash
-        serialize :parameters, Hash unless [:json, :jsonb, :hstore].include?(columns_hash['parameters'].type)
+        if table_exists? && !columns_hash['parameters'].type.in?([:json, :jsonb, :hstore])
+          serialize :parameters, Hash
+        end
 
         if ::ActiveRecord::VERSION::MAJOR < 4 || defined?(ProtectedAttributes)
           attr_accessible :key, :owner, :parameters, :recipient, :trackable