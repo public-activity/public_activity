4500aebb73c66e6e3834d1900c1756f54d93a00c
Tobias Matthies <me@tobiasmatthies.de>
2022-04-22 13:29:21 +0200

    Remove symlink, closes #319
    This fixes an issue when installing the Gem under Microsoft
    Windows, as File.symlink requires Administrator priviledges

--- test/migrations/001_create_activities.rb
+++ test/migrations/001_create_activities.rb
@@ -1,1 +1,25 @@
-../../lib/generators/public_activity/migration/templates/migration.rb
+# frozen_string_literal: true
+
+# Migration responsible for creating a table with activities
+class CreateActivities < (ActiveRecord.version.release() < Gem::Version.new('5.2.0') ? ActiveRecord::Migration : ActiveRecord::Migration[5.2])
+  # Create table
+  def self.up
+    create_table :activities do |t|
+      t.belongs_to :trackable, :polymorphic => true
+      t.belongs_to :owner, :polymorphic => true
+      t.string  :key
+      t.text    :parameters
+      t.belongs_to :recipient, :polymorphic => true
+
+      t.timestamps
+    end
+
+    add_index :activities, [:trackable_id, :trackable_type]
+    add_index :activities, [:owner_id, :owner_type]
+    add_index :activities, [:recipient_id, :recipient_type]
+  end
+  # Drop table
+  def self.down
+    drop_table :activities
+  end
+end
